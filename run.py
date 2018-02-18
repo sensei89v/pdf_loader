d9af8ca03d33ff8e92dc186ac5159e3b62daaeff
Sensei <sensei89v@gmail.com>
2018-02-19 01:12:33 +0300

    Добавлена проверка порта

--- run.py
+++ run.py
@@ -10,7 +10,16 @@ if __name__ == "__main__":
         print(USAGE)
         sys.exit(1)
 
-    port = int(argv[1])
+    try:
+        port = int(argv[1])
+    except:
+        print("port should be between 0 and 65535")
+        sys.exit(1)
+
+    if port < 0 and port > 65535:
+        print("port should be between 0 and 65535")
+        sys.exit(1)
+
     path_to_db = argv[2]
     template_dir = os.path.abspath('templates')
     server = Server(port, path_to_db, template_dir)
